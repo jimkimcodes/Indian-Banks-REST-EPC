083474b2d9a36e183b2f43e6e1413ef3bd117710
eswarclynn <esuclintblazer@gmail.com>
2020-07-03 00:43:51 +0530

    SQL file configured

--- indian_banks_final.sql
+++ indian_banks_final.sql
@@ -1,3 +1,46 @@
+CREATE TABLE banks (
+    name character varying(49),
+    id bigint NOT NULL
+);
+
+--
+-- Name: branches; Type: TABLE; Schema: public; Owner: siva; Tablespace: 
+--
+
+CREATE TABLE branches (
+    ifsc character varying(11) NOT NULL,
+    bank_id bigint,
+    branch character varying(74),
+    address character varying(195),
+    city character varying(50),
+    district character varying(50),
+    state character varying(26)
+);
+
+
+--
+-- Name: bank_branches; Type: VIEW; Schema: public; Owner: siva
+--
+
+CREATE VIEW bank_branches AS
+ SELECT branches.ifsc,
+    branches.bank_id,
+    branches.branch,
+    branches.address,
+    branches.city,
+    branches.district,
+    branches.state,
+    banks.name AS bank_name
+   FROM (branches
+     JOIN banks ON ((branches.bank_id = banks.id)));
+
+--
+-- Data for Name: banks; Type: TABLE DATA; Schema: public; Owner: siva
+--
+
+
+
+
 COPY banks (name, id) FROM stdin;
 STATE BANK OF INDIA	1
 PUNJAB NATIONAL BANK	2
@@ -2315,3 +2358,24 @@ ZSBL0000321	101	KHORA	KHODA COLONY KHODA	GHAZIABAD	GHAZIABAD	UTTAR PRADESH
 ZSBL0000331	101	RAVALI KALA	VILL.RAVLIKALA,MURAD NAGAR	RAVLI	GHAZIABAD	UTTAR PRADESH
 ZSBL0000341	101	LONI	HANSH AUTO MOBILS LONI GHAZIABAD	LONI	GHAZIABAD	UTTAR PRADESH
 \.
+
+
+
+ALTER TABLE ONLY banks
+    ADD CONSTRAINT banks_id_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: branches_ifsc_pkey; Type: CONSTRAINT; Schema: public; Owner: siva; Tablespace: 
+--
+
+ALTER TABLE ONLY branches
+    ADD CONSTRAINT branches_ifsc_pkey PRIMARY KEY (ifsc);
+
+
+--
+-- Name: branches_banks_fkey; Type: FK CONSTRAINT; Schema: public; Owner: siva
+--
+
+ALTER TABLE ONLY branches
+    ADD CONSTRAINT branches_banks_fkey FOREIGN KEY (bank_id) REFERENCES banks(id);
